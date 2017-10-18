8a7bc38854929f1ac63c2d0a5e1fa95a00739d1c
Megan <32867010+MeganPie@users.noreply.github.com>
2017-10-19 08:52:15 +1000

    Create language.php

---
+++ ExampleModule/language/EnglishUK/language.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ *	Made by MeganPie :: https://meganpie.github.io
+ *  Based off the Web Server Stats Module, created by Samerton.
+ *
+ *  EnglishUK Language file
+ */
+// Which version of NamelessMC is this language file updated to?
+$language_version = '2.0.0-dev';
+ 
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'example_module_title' => 'Example Module',
+	'example_module_desc' => 'This is the example module AdminCP Page! Feel free to use this as your base for your modules configs!!'
+);
