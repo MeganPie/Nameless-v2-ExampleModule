641037fec3efb5f4e92c7ac6e03c97f63127d51a
Megan <32867010+MeganPie@users.noreply.github.com>
2017-10-19 08:26:46 +1000

    Create module.php

---
+++ ExampleModule/module.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+ *	Made by MeganPie :: https://meganpie.github.io
+ *  Based off the Web Server Stats Module, created by Samerton.
+ *
+ *  Module information file
+ */
+$module_author = 'MeganPie';
+$module_version = 'v1.0.0';
+$nameless_version = '2.0.0-dev';
